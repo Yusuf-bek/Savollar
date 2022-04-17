9d66bde5bfecf3f19cdc85f185040df5231e5af6
Yusuf-bek <ogabeksherakhmatov@gmail.com>
2022-04-17 18:56:18 +0500

    added sizeConfig

---
+++ lib/core/components/size_config/size_config.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SizeConfig {
+  static MediaQueryData? _mediaQueryData;
+  static double? screenWidth;
+  static double? screenHeight;
+  static double? defaultSize;
+  static Orientation? orientation;
+
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData!.size.width;
+    screenHeight = _mediaQueryData!.size.height;
+    orientation = _mediaQueryData!.orientation;
+  }
+}
+
+double getHeightConfig(double inputHeight) {
+  double screenHeight = SizeConfig.screenHeight!;
+
+  return (inputHeight / 853.0) * screenHeight;
+}
+
+double getWidthConfig(double inputWidth) {
+  double screenWidth = SizeConfig.screenWidth!;
+
+  return (inputWidth / 411.0) * screenWidth;
+}
