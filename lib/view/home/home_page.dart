8bb3e3ec1084e170e843c6a571e041d6d4755151
Yusuf-bek <ogabeksherakhmatov@gmail.com>
2022-04-18 05:21:37 +0500

    completed home page ui

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -10,75 +10,87 @@ class HomePage extends StatelessWidget {
     SizeConfig().init(context);
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: getHeightConfig(50),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  CircleAvatar(
-                    backgroundColor: Colors.green.withOpacity(0.4),
-                    child: IconButton(
-                      onPressed: (() {}),
-                      icon: Icon(
-                        Icons.book,
-                        color: Colors.black.withOpacity(0.7),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: getHeightConfig(50),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.green.withOpacity(0.4),
+                      child: IconButton(
+                        onPressed: (() {}),
+                        icon: Icon(
+                          Icons.book,
+                          color: Colors.black.withOpacity(0.7),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    width: getWidthConfig(10),
-                  ),
-                  CircleAvatar(
-                    backgroundColor: Colors.green.withOpacity(0.4),
-                    child: IconButton(
-                      onPressed: (() {}),
-                      icon: Icon(
-                        Icons.person,
-                        color: Colors.black.withOpacity(0.7),
+                    SizedBox(
+                      width: getWidthConfig(10),
+                    ),
+                    CircleAvatar(
+                      backgroundColor: Colors.green.withOpacity(0.4),
+                      child: IconButton(
+                        onPressed: (() {}),
+                        icon: Icon(
+                          Icons.person,
+                          color: Colors.black.withOpacity(0.7),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    width: getWidthConfig(10),
-                  ),
+                    SizedBox(
+                      width: getWidthConfig(10),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: getHeightConfig(15),
+              ),
+              Text(
+                "Keling o'ynaymiz!",
+                style: TextStyle(
+                  color: Colors.orange,
+                  fontWeight: FontWeight.w500,
+                  fontSize: getWidthConfig(33),
+                ),
+              ),
+              CategoryContainer(
+                colorsToGradient: const [
+                  Colors.green,
+                  Colors.blue,
                 ],
+                assetImagePath: "assets/category_icons/biology.png",
+                nameCategory: "Chemistry",
+                onPressedPlayIcon: (){},
               ),
-            ),
-            SizedBox(
-              height: getHeightConfig(15),
-            ),
-            Text(
-              "Keling o'ynaymiz!",
-              style: TextStyle(
-                color: Colors.orange,
-                fontWeight: FontWeight.w500,
-                fontSize: getWidthConfig(33),
+              CategoryContainer(
+                colorsToGradient: const [
+                  
+                  Colors.green,
+                  Colors.blue,
+                ],
+                assetImagePath: "assets/category_icons/biology.png",
+                nameCategory: "Biology",
+                onPressedPlayIcon: (){},
               ),
-            ),
-            CategoryContainer(
-              colorsToGradient: const [
-                Colors.green,
-                Colors.blue,
-              ],
-              assetImagePath: "assets/category_icons/biology.png",
-              nameCategory: "Chemistry",
-              onPressedPlayIcon: (){},
-            ),
-            CategoryContainer(
-              colorsToGradient: const [
-                
-                Colors.green,
-                Colors.blue,
-              ],
-              assetImagePath: "assets/category_icons/biology.png",
-              nameCategory: "Biology",
-              onPressedPlayIcon: (){},
-            ),
-          ],
+              CategoryContainer(
+                colorsToGradient: const [
+                  Colors.green,
+                  Colors.blue,
+                ],
+                assetImagePath: "assets/category_icons/biology.png",
+                nameCategory: "Dart",
+                onPressedPlayIcon: (){},
+              ),
+              SizedBox(height: getHeightConfig(20),),
+            ],
+          ),
         ),
       ),
     );
